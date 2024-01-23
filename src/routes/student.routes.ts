99edeaa82b98bd0604bb3643cb5ecdd9fedf8667
Gabriel dos Santos <gabrields1910@gmail.com>
2024-01-23 20:23:26 -0300

    [feat: routes] Create student

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,8 +1,23 @@
 import { FastifyInstance } from 'fastify';
+import { StudantCreate } from '../interfaces/studants.interface';
 import { StudentUseCase } from '../uses-cases/studant.usecases';
 
 export async function studentRoutes(fastify: FastifyInstance) {
     const studentUseCase = new StudentUseCase()
+
+    fastify.post<{ Body: StudantCreate }>('/', async (req, reply) => {
+        const { cpf, name, ra } = req.body
+        try {
+            const data = await studentUseCase.create({
+                name,
+                cpf,
+                ra
+            })
+            return reply.send(data)
+        } catch (error) {
+            reply.send(error)
+        }
+    })
 
     fastify.get('/', (req, reply) => {
         reply.send({ hello: 'world' })
