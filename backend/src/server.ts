9a223a7afcbb10743d4038b24a33b54c20a5e94e
Gabriel dos Santos <gabrields1910@gmail.com>
2024-01-24 18:30:16 -0300

    Organize repository structure

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import Fastify from 'fastify';
+import { studentRoutes } from './routes/student.routes';
+
+const app = Fastify();
+
+app.register(studentRoutes, {
+    prefix: '/students',
+});
+
+app.listen({ port: 3100 }, () => {
+    console.log('----- Server listening on port 3100 -----');
+});
---
+++ backend/src/uses-cases/studant.usecases.ts
@@ -0,0 +1,28 @@
+import { Studant, StudantCreate, StudantRepository } from '../interfaces/studants.interface';
+import { StudantRepositoryPrisma } from '../repositories/student.repository';
+
+class StudentUseCase {
+    private studentRepository: StudantRepository;
+    constructor() {
+        this.studentRepository = new StudantRepositoryPrisma();
+    }
+
+    async create({ name, cpf, ra }: StudantCreate): Promise<Studant> {
+        const result = await this.studentRepository.create({ name, cpf, ra });
+
+        return result;
+    }
+
+    async listAllStudents(): Promise<Studant[]> {
+        const result = await this.studentRepository.listAllStudents()
+        return result
+    }
+
+    async delete(id: string) {
+        const data = await this.studentRepository.delete(id)
+        return data
+    }
+}
+
+export { StudentUseCase };
+
