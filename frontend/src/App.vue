efa789d2a76acfd78fb4ebaf2d9e61117a18af6e
Gabriel dos Santos <gabrields1910@gmail.com>
2024-01-28 19:10:35 -0300

    [feat: frontend] Create side menu and app bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,43 @@
 <template>
-  <v-app>
+  <v-app theme="dark">
+    <v-navigation-drawer v-model="isDrawerOpen">
+      <v-list>
+        <v-list-subheader>Módulo acadêmico</v-list-subheader>
+        <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+        <v-list-item prepend-icon="mdi-account" active>Alunos</v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar flat class="border-b">
+      <v-app-bar-nav-icon
+        @click="isDrawerOpen = !isDrawerOpen"
+      ></v-app-bar-nav-icon>
+        <img :src="logoSrc" height="50px" alt="my-logo" class="ml-3" />
+
+      <template #append>
+        <v-btn icon class="mr-2">
+          <v-badge dot color="info">
+            <v-icon icon="mdi-bell-outline"></v-icon>
+          </v-badge>
+        </v-btn>
+            <v-avatar>
+              <v-img
+                cover
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              ></v-img>
+            </v-avatar>
+         
+      </template>
+    </v-app-bar>
     <v-main>
-      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+import logoSrc from "../src/assets/logo-mais-a-educacao.png";
+const isDrawerOpen = ref(true);
   //
 </script>
